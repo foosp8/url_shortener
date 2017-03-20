6eae8c17eeef1c09d7827ba753f33b83fb890b09
foosp8 <foosp8@hotmail.com>
2017-03-19 23:01:37 -0700

    i 1. added validation in url.rb 2. created a file _form.html.erb 3. created a file called new.html.erb & link to urls/index.html.erb via "Shorten a Url" 4. defined the create action in urls_controller.rb 5. In urls_controller.rb, added a private method called url_params permits long_url to be exposed

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,7 @@
 class Url < ApplicationRecord
   validates :long_url, :presence =>true
-  before_create :shorten, :set
+  before_create :shorten
+  # :set
 
   def shorten
     self.short_url = SecureRandom.base64(5)
